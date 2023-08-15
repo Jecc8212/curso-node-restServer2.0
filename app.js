51decf1e2b137b4ef006d929086fd96c608062c3
Julian Carrasco <jecc_8212@gmail.com>
2023-08-15 18:00:21 -0400

    -env borrado

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+
+// const dotenv = require('dotenv')
+// const buf = Buffer.from('BASICO=basico')
+// const config = dotenv.parse(buf) // devolverá un objeto
+// console.log(typeof config, config) // objeto { BASICO : 'basico' }
+
+
+
 const Server = require('./models/server');
 
 
